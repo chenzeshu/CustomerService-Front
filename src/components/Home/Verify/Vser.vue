47a544b41af6a079aa723601874bc76c28687c6a
chenzeshu <1193297950@qq.com>
2018-01-12 17:25:03 +0800

    go on

--- src/components/Home/Verify/Vser.vue
+++ src/components/Home/Verify/Vser.vue
@@ -13,6 +13,7 @@
             <div class="middle-title">
               <Button class="select-button" @click="setStatus('待审核')" :type="type1">申请审核</Button>
               <Button class="select-button" @click="setStatus('申请完成')" :type="type2">申请完成</Button>
+              <Button class="select-button" @click="setStatus('申述中')" :type="type3">用户申述</Button>
             </div>
             <div class="right-title">
               <a href="#" style="color:#2d8cf0" @click.prevent="_getData">
@@ -23,8 +24,8 @@
         </div>
 
         <Split type="zhong"></Split>
-        <!--content-->
-        <div class="content">
+        <!--申请审核-->
+        <div class="content" v-if="status === '待审核'">
           <div class="list-item-wrapper" v-for="(item, key) in this.dataArr">
             <div class="list-item">
               <div class="item-left">
@@ -35,8 +36,8 @@
                   <br>
                   <br>
                   <p class="name1">提交人：{{ item.refer_man[0].name }}</p>
-                  <p class="name2">{{ item.created_at.substr(0, 10) }}</p>
-                  <p class="name2">{{ item.created_at.substr(11) }}</p>
+                  <p class="name2">{{ item.updated_at.substr(0, 10) }}</p>
+                  <p class="name2">{{ item.updated_at.substr(11) }}</p>
                 </div>
                 <div class="info1" v-if="item.customer[0]">
                   <span class="name" >客户联系人:{{ item.customer[0].name }}</span>
@@ -78,6 +79,124 @@
             <Split type="xi" v-show="key !== dataArr.length - 1"></Split>
           </div>
         </div>
+
+        <lazy-component>
+          <!--申请完成-->
+          <div class="content"  v-if="status === '申请完成'">
+            <div class="list-item-wrapper" v-for="(item, key) in this.dataArr">
+              <div class="list-item">
+                <div class="item-left">
+                  <div class="avatar">
+                    <img src="/static/avatar.png" alt="" width="64" height="64" style="cursor: pointer"
+                         @mouseover="showManDetail(key, $event)"
+                         @mouseleave="closeDetail">
+                    <br>
+                    <br>
+                    <p class="name1">提交员工：<span v-for="workman in item.workman">[{{workman.name}}] </span></p>
+                    <p class="name2">{{ item.updated_at.substr(0, 10) }}</p>
+                    <p class="name2">{{ item.updated_at.substr(11) }}</p>
+                  </div>
+                  <div class="info1" v-if="item.customer[0]">
+                    <span class="name" >客户联系人:{{ item.customer[0].name }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="iphone"></Icon>
+                      </div>
+                      联系方式: {{ item.customer[0].phone }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="android-document"></Icon>
+                      </div>
+                      所属合同: {{ item.contract && item.contract.name }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="man"></Icon>
+                      </div>
+                      项目经理: <span v-for="pm in item.project_manager">{{pm.name}}　</span></span>
+                    <span v-if="item.source[0]">
+                      <div class="icon">
+                        <Icon type="location"></Icon>
+                      </div>
+                      来源:{{ item.source[0].name }}</span>
+                    <span v-if="item.type[0]">
+                      <div class="icon">
+                        <Icon type="flag"></Icon>
+                      </div>
+                      类型:{{ item.type[0].name }}</span>
+                  </div>
+                  <div class="info2">
+
+                  </div>
+                </div>
+
+                <div class="verify">
+                  <i-button type="default" size="large" @click="_toggleDesc(key)">查看描述</i-button>
+                  <i-button type="primary" size="large" @click="_passAsk(item.id)">通过审核</i-button>
+                  <i-button type="error" size="large" @click="_rejAsk(item.id)">拒绝</i-button>
+                </div>
+              </div>
+              <Split type="xi" v-show="key !== dataArr.length - 1"></Split>
+            </div>
+          </div>
+
+          <!--申述-->
+          <div class="content"  v-if="status === '申述中'">
+            <div class="list-item-wrapper" v-for="(item, key) in this.dataArr">
+              <div class="list-item">
+                <div class="item-left">
+                  <div class="avatar">
+                    <img src="/static/avatar.png" alt="" width="64" height="64" style="cursor: pointer"
+                         @mouseover="showManDetail(key, $event)"
+                         @mouseleave="closeDetail">
+                    <br>
+                    <br>
+                    <p class="name1">申述人：[{{item.customer[0].name}}]</p>
+                    <p class="name2">{{ item.updated_at.substr(0, 10) }}</p>
+                    <p class="name2">{{ item.updated_at.substr(11) }}</p>
+                  </div>
+                  <div class="info1" v-if="item.customer[0]">
+                    <span class="name" >客户联系人:{{ item.customer[0].name }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="iphone"></Icon>
+                      </div>
+                      联系方式: {{ item.customer[0].phone }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="android-document"></Icon>
+                      </div>
+                      所属合同: {{ item.contract && item.contract.name }}</span>
+                    <span>
+                      <div class="icon">
+                        <Icon type="man"></Icon>
+                      </div>
+                      项目经理: <span v-for="pm in item.project_manager">{{pm.name}}　</span></span>
+                    <span v-if="item.source[0]">
+                      <div class="icon">
+                        <Icon type="location"></Icon>
+                      </div>
+                      来源:{{ item.source[0].name }}</span>
+                    <span v-if="item.type[0]">
+                      <div class="icon">
+                        <Icon type="flag"></Icon>
+                      </div>
+                      类型:{{ item.type[0].name }}</span>
+                  </div>
+                  <div class="info2">
+
+                  </div>
+                </div>
+
+                <div class="verify">
+                  <i-button type="default" size="large" @click="_toggleAllege(key)">申述详情</i-button>
+                  <i-button type="primary" size="large" @click="_passAsk(item.id)">想好</i-button>
+                  <i-button type="error" size="large" @click="_rejAsk(item.id)">怎么处理</i-button>
+                </div>
+              </div>
+              <Split type="xi" v-show="key !== dataArr.length - 1"></Split>
+            </div>
+          </div>
+        </lazy-component>
       </div>
 
       <Loading :loading="loading"></Loading>
@@ -97,6 +216,43 @@
                 </div>
             手机: <span>{{ curDetail.phone }}</span></span>
       </ShowDetail>
+
+      <!--显示申请完成详情-->
+      <Modal
+        v-model="descFlag"
+        width="400"
+        height="400"
+      >
+        <h2 style="font-size: 18px;font-weight: 700;height: 36px;line-height: 36px;">服务问题描述</h2>
+        <p style="font-size:16px;font-weight:500;line-height:36px;">{{desc1}}</p>
+        <Split type="xi"></Split>
+        <h2 style="font-size: 18px;font-weight: 700;height: 36px;line-height: 36px;">服务处理描述</h2>
+        <p style="font-size:16px;font-weight:500;line-height:36px;">{{desc2}}</p>
+        <Split type="xi"></Split>
+        <br>
+        <i-button type="primary" size="large" @click="_toggleImg(imgUrl)" v-if="imgUrl">展示图片</i-button>
+        <i-button type="default" size="large" v-else>没有附件</i-button>
+      </Modal>
+
+      <!--申述中详情-->
+      <Modal
+        v-model="allegeFlag"
+        width="400"
+        height="400"
+      >
+        <h2 style="font-size: 18px;font-weight: 700;height: 36px;line-height: 36px;">申述详情</h2>
+        <p style="font-size:16px;font-weight:500;line-height:36px;">{{allege}}</p>
+
+      </Modal>
+
+      <!--展示附件图片-->
+      <Modal
+        v-model="imgFlag"
+        width="600"
+        height="600"
+      >
+        <img :src="imgUrl" alt="">
+      </Modal>
     </div>
 </template>
 
@@ -105,6 +261,7 @@
   import {curdMixin} from 'common/js/mixin'
   import ShowDetail from 'base/Show/ShowDetail'
   import Loading from 'base/Loading/Loading'
+  import Url from 'common/js/url'
   export default {
         mixins:[curdMixin],
         data(){
@@ -120,7 +277,16 @@
               },
             status:"待审核",
             type1:'primary',
-            type2:null
+            type2:null,
+            type3:null,
+            //申请完成的图片
+            descFlag:false,
+            imgFlag: false,
+            allegeFlag:false,
+            imgUrl:"",
+            desc1:"",
+            desc2:"",
+            allege:""
           }
         },
         created(){
@@ -135,6 +301,43 @@
           },
           closeDetail(){
             this.$refs.showDetail.closeDetail()
+          },
+          _toggleAllege(index){
+            this.allegeFlag = !this.allegeFlag
+            this.allege = this.dataArr[index].allege
+          },
+          _toggleDesc(index){
+            this.descFlag = !this.descFlag
+            this.desc1 = this.dataArr[index].desc1
+            this.desc2 = this.dataArr[index].desc2
+            this.imgUrl = this.dataArr[index].img && this.dataArr[index].img.path.replace('public', Url.storage)
+          },
+          _toggleImg(){
+              this.imgFlag = !this.imgFlag
+          },
+          _passAsk(id){
+            this.$http
+              .get(`/${this.url}/passFinish/${id}`)
+              .then(res=>{
+                if(parseInt(res.data.code) === 7006){
+                  this.$Message.success(res.data.msg);
+                  this._getData()
+                }else{
+                  this.$Message.error('失败');
+                }
+              })
+          },
+          _rejAsk(id){
+            this.$http
+              .get(`/${this.url}/rejectFinish/${id}`)
+              .then(res=>{
+                if(parseInt(res.data.code) === -7006){
+                  this.$Message.success(res.data.msg);
+                  this._getData()
+                }else{
+                  this.$Message.error('失败');
+                }
+              })
           },
           _pass(id){
               this.$http
@@ -161,15 +364,20 @@
               })
           },
           setStatus(status){
-              this.status = status
-              if(status === "申请完成"){
-                  this.type1 = null
-                this.type2 = "primary"
-              }else{
+            this.status = status
+            this.type1 = this.type2 = this.type3 = null
+            switch (status){
+              case "待审核":
                 this.type1 = "primary"
-                this.type2 = null
-              }
-              this._getData()
+                  break
+              case "申请完成":
+                this.type2 = "primary"
+                break
+              case "申述中":
+                this.type3 = "primary"
+                break
+            }
+            this._getData()
           },
           _getData(){
             this._setLoading()
@@ -267,7 +475,7 @@
             .info2
               flex 0 0 300px
           .verify
-            flex 0 0 160px
+            flex 0 0 260px
             width 105px
             margin-top 10px
     .show-man-detail
